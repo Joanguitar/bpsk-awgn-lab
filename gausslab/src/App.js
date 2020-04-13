b3e7c1f43120b899767db8838ac93b1e4ff19e5f
Joan Palacios <jopabel93@gmail.com>
2020-04-14 01:42:16 +0200

    updates
    Also removed stupid files that were not in .gitignore

--- gausslab/src/App.js
+++ gausslab/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Scatter } from 'react-chartjs-2';
+import { sum } from 'mathjs'
 import logo from './logo.svg';
 import './App.css';
 
@@ -29,18 +30,23 @@ class App extends React.Component {
     super(props)
     this.state = {
       bits: new Array(12).fill(false),
-      signal: new Array(120).fill(0),
-      symbols_per_bit: 10,
+      signal: new Array(180).fill(0.0),
+      signal_filtered: new Array(60).fill(0.0),
       pulse: new Array(10).fill(1),
+      filter: new Array(10).fill(0.1),
       count: 0
     }
   }
-  add2signal = (value) => {
-    console.log(last(this.state.signal));
+  add2signals = (value) => {
     var signal = this.state.signal
+    var signal_filtered = this.state.signal_filtered
     signal.push(value ? 1 : -1)
     signal.shift()
-    this.setState({signal: signal})
+    signal_filtered.push(
+      sum(signal.slice(0, this.state.filter.length).map((value, ii) => value*this.state.filter[ii]))
+    )
+    signal_filtered.shift()
+    this.setState({signal: signal, signal_filtered: signal_filtered})
   }
   add2bits = (value) => {
     var bits = this.state.bits
@@ -49,7 +55,7 @@ class App extends React.Component {
     this.setState({bits: bits})
   }
   update = () => {
-    this.add2signal(last(this.state.bits)*this.state.pulse[this.state.count])
+    this.add2signals(last(this.state.bits)*this.state.pulse[this.state.count])
     if (this.state.count == 9) {
       this.add2bits(Math.random() >= 0.5)
       this.setState({count: 0})
@@ -58,7 +64,7 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    this.interval = setInterval(() => this.update(), 100);
+    this.interval = setInterval(() => this.update(), 50);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -70,7 +76,7 @@ class App extends React.Component {
           <Col lg="8">
             <Card>
               <CardBody>
-                <Line
+                <Scatter
                   data={{
                     datasets: [
                       {
@@ -80,15 +86,15 @@ class App extends React.Component {
                         lineTension: 0.1,
                         backgroundColor: "rgba(255, 0, 0, 0.1)",
                         borderColor: "#f11e1f",
-                        borderWidth: 5,
+                        borderWidth: 2,
                         borderDash: [],
                         borderDashOffset: 0.0,
-                        pointRadius: 4,//4,
-                        data: this.state.signal.slice().reverse(),
+                        pointRadius: 3,//4,
+                        data: this.state.signal.slice().reverse().map((simbol, ii) => {return({x: ii, y: simbol})}),
                       }
                     ]
                   }}
-                  options = {{
+                  options={{
                     animation: {
                         duration: 0.05
                     },
@@ -102,33 +108,108 @@ class App extends React.Component {
                     scales: {
                       yAxes: [
                         {
+                          barPercentage: 1.6,
                           gridLines: {
-                            display: true,
                             drawBorder: false,
-                            color: "rgba(255,255,255,0.1)",
+                            color: "rgba(29,140,248,0.0)",
                             zeroLineColor: "transparent"
                           },
                           ticks: {
                             min: -2,
                             max: 2,
-                            fontColor: "#9a9a9a",
-                            stepSize: 0.5,
+                            padding: 20,
+                            fontColor: "#9e9e9e",
+                            stepSize: 1
                           }
                         }
                       ],
+
                       xAxes: [
                         {
+                          barPercentage: 1.6,
                           gridLines: {
-                            display: true,
                             drawBorder: false,
-                            color: "rgba(255,255,255,0.1)",
+                            color: "rgba(0,242,195,0.1)",
                             zeroLineColor: "transparent"
                           },
                           ticks: {
+                            padding: 20,
                             min: 0,
-                            max: 120,
-                            fontColor: "#9a9a9a",
-                            stepSize: 1,
+                            max: 180,
+                            fontColor: "#9e9e9e"
+                          }
+                        }
+                      ]
+                    }
+                  }}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="4">
+            <Card>
+              <CardBody>
+                <Scatter
+                  data={{
+                    datasets: [
+                      {
+                        label: "signal",
+                        fill: true,
+                        showLine: true,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(255, 0, 0, 0.1)",
+                        borderColor: "#f11e1f",
+                        borderWidth: 2,
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        pointRadius: 3,//4,
+                        data: this.state.signal_filtered.slice().reverse().map((simbol, ii) => {return({x: ii, y: simbol})}),
+                      }
+                    ]
+                  }}
+                  options={{
+                    animation: {
+                        duration: 0.05
+                    },
+                    legend: {
+                      display: false
+                    },
+                    tooltips: {
+                      enabled: false,
+                    },
+                    responsive: true,
+                    scales: {
+                      yAxes: [
+                        {
+                          barPercentage: 1.6,
+                          gridLines: {
+                            drawBorder: false,
+                            color: "rgba(29,140,248,0.0)",
+                            zeroLineColor: "transparent"
+                          },
+                          ticks: {
+                            min: -2,
+                            max: 2,
+                            padding: 20,
+                            fontColor: "#9e9e9e",
+                            stepSize: 1
+                          }
+                        }
+                      ],
+
+                      xAxes: [
+                        {
+                          barPercentage: 1.6,
+                          gridLines: {
+                            drawBorder: false,
+                            color: "rgba(0,242,195,0.1)",
+                            zeroLineColor: "transparent"
+                          },
+                          ticks: {
+                            padding: 20,
+                            min: 0,
+                            max: 60,
+                            fontColor: "#9e9e9e"
                           }
                         }
                       ]
